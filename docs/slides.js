b2859945cf39ac06d9d3c6d6ab80e4c5efc4bc9d
Antoine07 <antoine.lucsko@wanadoo.fr>
2026-06-14 23:06:38 +0200

    Move gradient descent to course end

--- docs/slides.js
+++ docs/slides.js
@@ -16,10 +16,10 @@ let menuButtons = [];
 const courseSections = [
   { title: "Introduction", start: 0, end: 6 },
   { title: "Préparer les données", start: 7, end: 10 },
-  { title: "Régression linéaire", start: 11, end: 23 },
-  { title: "Perceptron", start: 24, end: 24 },
-  { title: "KNN", start: 25, end: 38 },
-  { title: "Exercices", start: 39, end: 39 },
+  { title: "Régression linéaire", start: 11, end: 21 },
+  { title: "KNN", start: 22, end: 35 },
+  { title: "Exercices", start: 36, end: 36 },
+  { title: "Optimisation", start: 37, end: 38 },
 ];
 
 function clampIndex(index) {
